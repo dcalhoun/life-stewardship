e2d94df875bea55e649aba3d77fa2e552126b91a
David Calhoun <dpcalhoun@gmail.com>
2020-06-27 17:45:26 -0500

    Improve page hierarchy

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,61 +3,63 @@ import Navigation from "../components/Navigation";
 
 export default function Index() {
   return (
-    <main>
+    <>
       <Head>
         <title>Contact - Life Stewardship</title>
       </Head>
       <Navigation />
-      <h1>Contact</h1>
-      <address>
-        <dl>
-          <dt>Mail</dt>
-          <dd>
-            1888 Main Street, Suite C-198
-            <br />
-            Madison, MS 39110
-          </dd>
-          <dt>Phone</dt>
-          <dd>(601)-624-5135</dd>
-          <dt>Email</dt>
-          <dd>
-            <a href="mailto:paul@pcalhoun.com">paul@pcalhoun.com</a>
-          </dd>
-        </dl>
-      </address>
-      <form action="">
-        <div>
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" required />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" required />
-        </div>
-        <div>
-          <label htmlFor="phone">Phone</label>
-          <input type="tel" name="phone" id="phone" />
-        </div>
-        <div>
-          <label htmlFor="fax">Fax</label>
-          <input type="tel" name="fax" id="fax" />
-        </div>
-        <div>
-          <label htmlFor="address">Address</label>
-          <input type="text" name="address" id="address" />
-        </div>
-        <div>
-          <label htmlFor="message">Message</label>
-          <textarea
-            name="message"
-            id="message"
-            cols={30}
-            rows={10}
-            required
-          ></textarea>
-        </div>
-        <button type="submit">Send Message</button>
-      </form>
-    </main>
+      <main>
+        <h1>Contact</h1>
+        <address>
+          <dl>
+            <dt>Mail</dt>
+            <dd>
+              1888 Main Street, Suite C-198
+              <br />
+              Madison, MS 39110
+            </dd>
+            <dt>Phone</dt>
+            <dd>(601) 624-5135</dd>
+            <dt>Email</dt>
+            <dd>
+              <a href="mailto:paul@pcalhoun.com">paul@pcalhoun.com</a>
+            </dd>
+          </dl>
+        </address>
+        <form action="">
+          <div>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" required />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" required />
+          </div>
+          <div>
+            <label htmlFor="phone">Phone</label>
+            <input type="tel" name="phone" id="phone" />
+          </div>
+          <div>
+            <label htmlFor="fax">Fax</label>
+            <input type="tel" name="fax" id="fax" />
+          </div>
+          <div>
+            <label htmlFor="address">Address</label>
+            <input type="text" name="address" id="address" />
+          </div>
+          <div>
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              cols={30}
+              rows={10}
+              required
+            ></textarea>
+          </div>
+          <button type="submit">Send Message</button>
+        </form>
+      </main>
+    </>
   );
 }
--- pages/nonprofit.tsx
+++ pages/nonprofit.tsx
@@ -3,40 +3,42 @@ import Navigation from "../components/Navigation";
 
 export default function Index() {
   return (
-    <main>
+    <>
       <Head>
         <title>Nonprofit - Life Stewardship</title>
       </Head>
       <Navigation />
-      <h1>Nonprofit</h1>
-      <p>
-        Effective nonprofit organizations are mission driven. Their management
-        and board of directors recognize the importance of having a sound
-        accounting and financial reporting system to help them to fulfill their
-        fiduciary responsibility to donors and make good business decisions. We
-        have extensive experience working with nonprofit organizations and
-        recognize the unique characteristics of nonprofit organizations and
-        their board of directors.
-      </p>
+      <main>
+        <h1>Nonprofit</h1>
+        <p>
+          Effective nonprofit organizations are mission driven. Their management
+          and board of directors recognize the importance of having a sound
+          accounting and financial reporting system to help them to fulfill
+          their fiduciary responsibility to donors and make good business
+          decisions. We have extensive experience working with nonprofit
+          organizations and recognize the unique characteristics of nonprofit
+          organizations and their board of directors.
+        </p>
 
-      <p>
-        If you are looking for ways to strengthen your organization with
-        professionals who have a comprehensive understanding of operational
-        issues, tax compliance and fiduciary responsibilities unique to
-        nonprofit organizations, let’s talk. We can assist you with:
-      </p>
+        <p>
+          If you are looking for ways to strengthen your organization with
+          professionals who have a comprehensive understanding of operational
+          issues, tax compliance and fiduciary responsibilities unique to
+          nonprofit organizations, let’s talk. We can assist you with:
+        </p>
 
-      <ul>
-        <li>Accounting System Conversion and Implementation</li>
-        <li>Training for accounting staff</li>
-        <li>Internal Controls</li>
-        <li>Audit Preparation</li>
-        <li>Financial Statement Preparation</li>
-        <li>Grant Compliance</li>
-        <li>Training for Board of Director </li>
-        <li>Assisting with Board Governance & Oversite</li>
-        <li>Strategic Financial Planning</li>
-      </ul>
-    </main>
+        <ul>
+          <li>Accounting System Conversion and Implementation</li>
+          <li>Training for accounting staff</li>
+          <li>Internal Controls</li>
+          <li>Audit Preparation</li>
+          <li>Financial Statement Preparation</li>
+          <li>Grant Compliance</li>
+          <li>Training for Board of Director </li>
+          <li>Assisting with Board Governance & Oversite</li>
+          <li>Strategic Financial Planning</li>
+        </ul>
+      </main>
+    </>
   );
 }
