4719c0a44c27bf522115f4bb24d144963d3a88a8
David Calhoun <dpcalhoun@gmail.com>
2020-06-27 15:58:07 -0500

    Add project structure

---
+++ README.md
@@ -0,0 +1,8 @@
+# Life Stewardship
+
+Marketing website for Life Stewardship LLC, financial planning & coaching.
+
+## Project Set Up
+
+1. `yarn install`
+1. `yarn dev`
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Index() {
+  return (
+    <main>
+      <Head>
+        <title>Life Stewardship</title>
+      </Head>
+      <h1>Life Stewardship</h1>
+    </main>
+  );
+}
