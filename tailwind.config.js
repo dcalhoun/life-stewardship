3f5b8d40ddf03ffd4cca41486bd9597c28034eec
David Calhoun <dpcalhoun@gmail.com>
2020-11-27 13:59:35 -0600

    Upgrade dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: ["./pages/**/*.{ts,tsx,js,re}", "./components/**/*.{ts,tsx,js,re}"],
-  theme: {},
   variants: {
     accessibility: ({ after }) => after(["focus-within"]),
   },
-  plugins: [],
 };
