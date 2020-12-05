835917580e04cca08c73829bd93acf1d66617e45
David Calhoun <dpcalhoun@gmail.com>
2020-12-05 17:32:45 -0600

    Update Next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-const withTM = require("next-transpile-modules")(["bs-platform"]);
+const bsconfig = require("./bsconfig.json");
+
+const withTM = require("next-transpile-modules")(
+  ["bs-platform"].concat(bsconfig["bs-dependencies"])
+);
 
 module.exports = withTM({
   pageExtensions: ["jsx", "js", "ts", "tsx", "bs.js"],
