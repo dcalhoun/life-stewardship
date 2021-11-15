5326cce3beeed7eeb7976ad86a0a807e0e675d16
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2021-11-14 18:07:50 -0600

    Fix index file after removing Blockbase parent them

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
 /**
- * The main template file
+ * Fallback if Gutenberg plugin is not installed.
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * @package
+ * @since 1.0
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Blockbase
- * @since 1.2.9
  */
+
 get_header();
 
-// the query
-echo gutenberg_block_template_part( 'index' );
+if (current_user_can("activate_plugins")) {
+    echo esc_html__(
+        "This theme requires the Gutenberg plugin to be installed.",
+        "blockbase",
+    );
+}
 
 get_footer();
