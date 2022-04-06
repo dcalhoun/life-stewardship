9b4de9daf57ca178ebe3dc57fa5d5eb56e4ddac2
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-04-06 08:19:51 -0500

    Add Affiliation pattern

---
+++ patterns/affiliation.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Affiliation
+ * Slug: life-stewardship/affiliation
+ */
+?>
+
+<!-- wp:columns {"verticalAlignment":null,"isStackedOnMobile":false,"style":{"spacing":{"padding":{"top":"40px","right":"40px","bottom":"40px","left":"40px"},"blockGap":{"top":"1.25rem","left":"1.25rem"}},"border":{"radius":"0.5rem"}},"backgroundColor":"background-secondary","className":"ls-drop-shadow"} -->
+<div class="wp-block-columns is-not-stacked-on-mobile ls-drop-shadow has-background-secondary-background-color has-background" style="border-radius:0.5rem;padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px"><!-- wp:column {"verticalAlignment":"center","width":"2.625rem","lock":{"move":true,"remove":true}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:2.625rem"><!-- wp:social-links {"align":"center","layout":{"type":"flex","justifyContent":"center"},"lock":{"move":true,"remove":true}} -->
+<ul class="wp-block-social-links aligncenter"><!-- wp:social-link {"service":"chain"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"100%","lock":{"move":true,"remove":true}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:100%"><!-- wp:paragraph {"placeholder":"Type affiliation name","textColor":"foreground-secondary","className":"ls-font-alternate","fontSize":"base","lock":{"move":true,"remove":true}} -->
+<p class="ls-font-alternate has-foreground-secondary-color has-text-color has-base-font-size"><em></em></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
