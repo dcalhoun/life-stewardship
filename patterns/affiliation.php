0d9413dc4cf99ae29e852453a87a66c55416648c
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-04-07 13:21:24 -0500

    Improve Affiliation pattern design

--- patterns/affiliation.php
+++ patterns/affiliation.php
@@ -5,11 +5,11 @@
  */
 ?>
 
-<!-- wp:columns {"verticalAlignment":null,"isStackedOnMobile":false,"style":{"spacing":{"padding":{"top":"40px","right":"40px","bottom":"40px","left":"40px"},"blockGap":{"top":"1.25rem","left":"1.25rem"}},"border":{"radius":"0.5rem"}},"backgroundColor":"background-secondary","className":"ls-drop-shadow"} -->
-<div class="wp-block-columns is-not-stacked-on-mobile ls-drop-shadow has-background-secondary-background-color has-background" style="border-radius:0.5rem;padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px"><!-- wp:column {"verticalAlignment":"center","width":"2.625rem","lock":{"move":true,"remove":true}} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:2.625rem"><!-- wp:social-links {"align":"center","layout":{"type":"flex","justifyContent":"center"},"lock":{"move":true,"remove":true}} -->
-<ul class="wp-block-social-links aligncenter"><!-- wp:social-link {"service":"chain"} /--></ul>
-<!-- /wp:social-links --></div>
+<!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"top":"40px","right":"40px","bottom":"40px","left":"40px"},"blockGap":{"top":"1.25rem","left":"1.25rem"}},"border":{"radius":"0.5rem"}},"backgroundColor":"background-secondary","className":"ls-drop-shadow"} -->
+<div class="wp-block-columns is-not-stacked-on-mobile ls-drop-shadow has-background-secondary-background-color has-background" style="border-radius:0.5rem;padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px"><!-- wp:column {"verticalAlignment":"center","width":"48px","lock":{"move":true,"remove":true}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:48px"><!-- wp:image {"width":48,"height":48,"lock":{"move":true,"remove":true}} -->
+<figure class="wp-block-image is-resized"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/check.svg" alt="" width="48" height="48"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center","width":"100%","lock":{"move":true,"remove":true}} -->
