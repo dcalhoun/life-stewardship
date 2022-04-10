89317e3fc31f1284b13fdaadab4f6c7bc6f6464f
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-04-10 10:57:15 -0500

    Fix Affiliation responsive styles

--- patterns/affiliation.php
+++ patterns/affiliation.php
@@ -6,8 +6,8 @@
 ?>
 
 <!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"top":"40px","right":"40px","bottom":"40px","left":"40px"},"blockGap":{"top":"1.25rem","left":"1.25rem"}},"border":{"radius":"0.5rem"}},"backgroundColor":"background-secondary","className":"ls-drop-shadow"} -->
-<div class="wp-block-columns is-not-stacked-on-mobile ls-drop-shadow has-background-secondary-background-color has-background" style="border-radius:0.5rem;padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px"><!-- wp:column {"verticalAlignment":"center","width":"48px","lock":{"move":true,"remove":true}} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:48px"><!-- wp:image {"width":48,"height":48,"lock":{"move":true,"remove":true}} -->
+<div class="wp-block-columns is-not-stacked-on-mobile ls-drop-shadow has-background-secondary-background-color has-background" style="border-radius:0.5rem;padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px"><!-- wp:column {"verticalAlignment":"center","width":"48px","lock":{"move":true,"remove":true},"className":"ls-shrink-0"} -->
+<div class="wp-block-column is-vertically-aligned-center ls-shrink-0" style="flex-basis:48px"><!-- wp:image {"width":48,"height":48,"lock":{"move":true,"remove":true}} -->
 <figure class="wp-block-image is-resized"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/check.svg" alt="" width="48" height="48"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
