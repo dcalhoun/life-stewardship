601d055c0a55e2405ed239e51d895181e261e424
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-04-11 12:29:41 -0500

    Format patterns

--- patterns/affiliation.php
+++ patterns/affiliation.php
@@ -5,16 +5,54 @@
  */
 ?>
 
-<!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"top":"2.5rem","right":"2.5rem","bottom":"2.5rem","left":"2.5rem"},"blockGap":{"top":"1.25rem","left":"1.25rem"}},"border":{"radius":"0.5rem"}},"backgroundColor":"background-secondary","className":"ls-drop-shadow"} -->
-<div class="wp-block-columns is-not-stacked-on-mobile ls-drop-shadow has-background-secondary-background-color has-background" style="border-radius:0.5rem;padding-top:2.5rem;padding-right:2.5rem;padding-bottom:2.5rem;padding-left:2.5rem"><!-- wp:column {"verticalAlignment":"center","width":"48px","lock":{"move":true,"remove":true},"className":"ls-shrink-0"} -->
-<div class="wp-block-column is-vertically-aligned-center ls-shrink-0" style="flex-basis:48px"><!-- wp:image {"width":48,"height":48,"lock":{"move":true,"remove":true}} -->
-<figure class="wp-block-image is-resized"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/check.svg" alt="" width="48" height="48"/></figure>
-<!-- /wp:image --></div>
-<!-- /wp:column -->
+<!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"top":"2.5rem","right":"2.5rem","bottom":"2.5rem","left":"2.5rem"}},"border":{"radius":"0.5rem"}},"backgroundColor":"background-secondary","className":"ls-drop-shadow"} -->
+<div
+  class="
+    wp-block-columns
+    is-not-stacked-on-mobile
+    ls-drop-shadow
+    has-background-secondary-background-color has-background
+  "
+  style="
+    border-radius: 0.5rem;
+    padding-top: 2.5rem;
+    padding-right: 2.5rem;
+    padding-bottom: 2.5rem;
+    padding-left: 2.5rem;
+  "
+>
+  <!-- wp:column {"verticalAlignment":"center","width":"48px","lock":{"move":true,"remove":true},"className":"ls-shrink-0"} -->
+  <div
+    class="wp-block-column is-vertically-aligned-center ls-shrink-0"
+    style="flex-basis: 48px"
+  >
+    <!-- wp:image {"width":48,"height":48,"lock":{"move":true,"remove":true}} -->
+    <figure class="wp-block-image is-resized">
+      <img
+        src="<?php echo get_stylesheet_directory_uri(); ?>/assets/check.svg"
+        alt=""
+        width="48"
+        height="48"
+      />
+    </figure>
+    <!-- /wp:image -->
+  </div>
+  <!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"center","width":"100%","lock":{"move":true,"remove":true}} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:100%"><!-- wp:paragraph {"placeholder":"Type affiliation name","textColor":"foreground-secondary","className":"ls-font-alternate","fontSize":"base","lock":{"move":true,"remove":true}} -->
-<p class="ls-font-alternate has-foreground-secondary-color has-text-color has-base-font-size"><em></em></p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column --></div>
+  <!-- wp:column {"verticalAlignment":"center","width":"100%","lock":{"move":true,"remove":true}} -->
+  <div
+    class="wp-block-column is-vertically-aligned-center"
+    style="flex-basis: 100%"
+  >
+    <!-- wp:paragraph {"placeholder":"Type affiliation name","textColor":"foreground-secondary","className":"ls-font-alternate","fontSize":"base","lock":{"move":true,"remove":true}} -->
+    <p
+      class="
+        ls-font-alternate
+        has-foreground-secondary-color has-text-color has-base-font-size
+      "
+    ></p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:column -->
+</div>
 <!-- /wp:columns -->
