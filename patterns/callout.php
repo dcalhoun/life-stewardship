1924f1c19a2d522bfcc87cab76b0975f3244f867
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-04-11 07:35:30 -0500

    Update pattern padding

--- patterns/callout.php
+++ patterns/callout.php
@@ -5,8 +5,8 @@
  */
 ?>
 
-<!-- wp:group {"style":{"border":{"radius":"0.5rem"},"spacing":{"padding":{"top":"2.5rem","right":"2.5rem","bottom":"2.5rem","left":"2.5rem"}}},"backgroundColor":"background-secondary","className":"ls-drop-shadow","layout":{"contentSize":"672px","wideSize":"864px"}} -->
-<div class="wp-block-group ls-drop-shadow has-background-secondary-background-color has-background" style="border-radius:0.5rem;padding-top:2.5rem;padding-right:2.5rem;padding-bottom:2.5rem;padding-left:2.5rem"><!-- wp:heading {"placeholder":"Primary heading","textAlign":"center","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"300","lineHeight":1.1},"spacing":{"margin":{"top":"0px","right":"0px","bottom":"20px","left":"0px"}}},"textColor":"secondary","className":"ls-font-alternate","fontSize":"large","lock":{"move":true,"remove":true}} -->
+<!-- wp:group {"style":{"border":{"radius":"0.5rem"},"spacing":{"padding":{"top":"2.5rem","right":"1rem","bottom":"2.5rem","left":"1rem"}}},"backgroundColor":"background-secondary","className":"ls-drop-shadow","layout":{"contentSize":"672px","wideSize":"864px"}} -->
+<div class="wp-block-group ls-drop-shadow has-background-secondary-background-color has-background" style="border-radius:0.5rem;padding-top:2.5rem;padding-right:1rem;padding-bottom:2.5rem;padding-left:1rem"><!-- wp:heading {"placeholder":"Primary heading","textAlign":"center","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"300","lineHeight":1.1},"spacing":{"margin":{"top":"0px","right":"0px","bottom":"20px","left":"0px"}}},"textColor":"secondary","className":"ls-font-alternate","fontSize":"large","lock":{"move":true,"remove":true}} -->
 <h1 class="has-text-align-center ls-font-alternate has-secondary-color has-text-color has-large-font-size" style="font-style:normal;font-weight:300;line-height:1.1;margin-top:0px;margin-right:0px;margin-bottom:20px;margin-left:0px"></h1>
 <!-- /wp:heading -->
 
