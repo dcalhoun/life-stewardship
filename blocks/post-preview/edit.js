567201769f14e5d9beae7c732bf88c630350025e
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-05-26 10:51:19 -0500

    feature: Display post title in Post Preview block

--- blocks/post-preview/edit.js
+++ blocks/post-preview/edit.js
@@ -1,3 +1,19 @@
-export default function Edit() {
-  return <p>Post Preview</p>;
+import { useEntityProp } from "@wordpress/core-data";
+
+export default function PostPreviewEdit({ context: { postType, postId } }) {
+  // @TODO: Why is useEntityProp undefined?
+  const [postTitle] = (useEntityProp || window.wp.coreData.useEntityProp)(
+    "postType",
+    postType,
+    "title",
+    postId
+  );
+
+  return (
+    <article>
+      <a>
+        <h2>{postTitle}</h2>
+      </a>
+    </article>
+  );
 }
