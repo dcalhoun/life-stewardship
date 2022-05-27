2bcc65419a8eb7c12ba8fe3b12b53c21ec75b026
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-05-27 08:29:09 -0500

    refactor: Rename post title property

--- blocks/post-preview/edit.js
+++ blocks/post-preview/edit.js
@@ -1,13 +1,13 @@
 import { useEntityProp } from "@wordpress/core-data";
 
 export default function PostPreviewEdit({ context: { postType, postId } }) {
-  const [postTitle] = useEntityProp("postType", postType, "title", postId);
+  const [title] = useEntityProp("postType", postType, "title", postId);
   const [link] = useEntityProp("postType", postType, "link", postId);
 
   return (
     <article>
       <a href={link}>
-        <h2>{postTitle}</h2>
+        <h2>{title}</h2>
       </a>
     </article>
   );
