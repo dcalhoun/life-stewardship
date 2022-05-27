a80e3c79c2cf25cf7a5555fbac1dc9ce5353c224
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-05-27 08:22:19 -0500

    feat: Add post link to Post Preview

--- blocks/post-preview/edit.js
+++ blocks/post-preview/edit.js
@@ -2,10 +2,11 @@ import { useEntityProp } from "@wordpress/core-data";
 
 export default function PostPreviewEdit({ context: { postType, postId } }) {
   const [postTitle] = useEntityProp("postType", postType, "title", postId);
+  const [link] = useEntityProp("postType", postType, "link", postId);
 
   return (
     <article>
-      <a>
+      <a href={link}>
         <h2>{postTitle}</h2>
       </a>
     </article>
