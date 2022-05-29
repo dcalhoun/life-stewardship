2455390b07a79d64fd3fc33a0392a3dfd7320e48
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-05-28 21:06:34 -0500

    feat: Display featured image

--- blocks/post-preview/edit.js
+++ blocks/post-preview/edit.js
@@ -1,4 +1,9 @@
-import { useEntityProp } from "@wordpress/core-data";
+import { useEntityProp, store as coreStore } from "@wordpress/core-data";
+import { useSelect } from "@wordpress/data";
+
+function getMediaSourceUrlBySizeSlug(media, slug) {
+  return media?.media_details?.sizes?.[slug]?.source_url || media?.source_url;
+}
 
 export default function PostPreviewEdit({ context: { postType, postId } }) {
   const [, , { rendered: title }] = useEntityProp(
@@ -9,6 +14,36 @@ export default function PostPreviewEdit({ context: { postType, postId } }) {
   );
   const [link] = useEntityProp("postType", postType, "link", postId);
   const [date] = useEntityProp("postType", postType, "date", postId);
+  const [imageId] = useEntityProp(
+    "postType",
+    postType,
+    "featured_media",
+    postId
+  );
+  const [iconId] = useEntityProp("root", "site", "site_icon");
+
+  const { image, icon } = useSelect(
+    (select) => {
+      const { getMedia } = select(coreStore);
+      return {
+        image: imageId && getMedia(imageId, { context: "view" }),
+        icon: iconId && getMedia(iconId, { context: "view" }),
+      };
+    },
+    [imageId, iconId]
+  );
+
+  const imageUrl = getMediaSourceUrlBySizeSlug(image, "thumbnail");
+  const iconUrl = getMediaSourceUrlBySizeSlug(icon, "thumbnail");
+
+  let imgUrl = "";
+  if (imageId && !image) {
+    imgUrl = iconUrl;
+  } else if (imageId && image) {
+    imgUrl = imageUrl;
+  } else if (iconId && icon) {
+    imgUrl = iconUrl;
+  }
 
   return (
     <article role="listitem">
@@ -17,7 +52,14 @@ export default function PostPreviewEdit({ context: { postType, postId } }) {
         onClick={(event) => event.preventDefault()}
         title={`${title} - Published on ${date}`}
       >
-        <h2 aria-hidden="true">{title}</h2>
+        <img
+          aria-hidden="true"
+          className="is-style-rounded"
+          height="100"
+          src={imgUrl}
+          width="100"
+        />
+        )<h2 aria-hidden="true">{title}</h2>
         <time aria-hidden="true">{date}</time>
       </a>
     </article>
