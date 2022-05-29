cbc4e622b727634ba13dab28018027a3b76972e9
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2022-05-28 20:28:45 -0500

    feat: Remove Post Preview save function
    The block is considered "dynamic" and therefore does not need a `save`
    function.

--- blocks/post-preview/index.js
+++ blocks/post-preview/index.js
@@ -1,6 +1,5 @@
 import { registerBlockType } from "@wordpress/blocks";
 import metadata from "./block.json";
 import edit from "./edit";
-import save from "./save";
 
-registerBlockType(metadata, { edit, save });
+registerBlockType(metadata, { edit });
